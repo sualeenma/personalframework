0d81d2a6a580da4c04df3aa6c6ae10d254c3456f
sualeenma <sualee@hawaii.edu>
2023-10-12 12:56:22 -1000

    edited

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// CHANGE ACTIVE STATE FOR ALL SECTIONS WITH INTERSECTION OBSERVER
+const myobserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.setAttribute("data-sectionstate", "active");
+      } else {
+        entry.target.setAttribute("data-sectionstate", "inactive");
+      }
+    });
+  });
+  
+  
+  document.querySelectorAll(".animate-me-on-scroll").forEach((section) => {
+    myobserver.observe(section);
+  });
+  
